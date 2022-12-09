4fbcc4b422d6e6e42eee7ad5c36109bade1047ea
Kevin <kasmeltz@lakeheadu.ca>
2022-12-09 12:04:11 -0500

    initial check in

---
+++ Unity/MiniJam121/Assets/Scripts/Behaviours/PlayerBehaviour.cs
@@ -0,0 +1,9 @@
+namespace HNS.MiniJam121.Unity.Behaviours
+{
+    using UnityEngine;
+
+    [AddComponentMenu("MJ121/Player")]
+    public class PlayerBehaviour : MonoBehaviour
+    {       
+    }
+}
