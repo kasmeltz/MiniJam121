c90de3c7381afee02f8f7bad182e4424099aea52
Kevin <kasmeltz@lakeheadu.ca>
2022-12-10 10:34:35 -0500

    nice

--- Unity/MiniJam121/Assets/Scripts/Behaviours/PlayerBehaviour.cs
+++ Unity/MiniJam121/Assets/Scripts/Behaviours/PlayerBehaviour.cs
@@ -4,6 +4,22 @@ namespace HNS.MiniJam121.Unity.Behaviours
 
     [AddComponentMenu("MJ121/Player")]
     public class PlayerBehaviour : MonoBehaviour
-    {       
+    {
+        public float MoveSpeed;
+
+        protected void Update()
+        {
+            if (Input.GetKey(KeyCode.A) || 
+                Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position -= new Vector3(MoveSpeed, 0, 0) * Time.deltaTime;
+            }
+
+            if (Input.GetKey(KeyCode.D) ||
+                Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += new Vector3(MoveSpeed, 0, 0) * Time.deltaTime;
+            }
+        }
     }
 }
