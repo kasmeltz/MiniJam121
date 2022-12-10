54638c3ea02cc0ec396798744c8f97de3181c646
Kevin <kasmeltz@lakeheadu.ca>
2022-12-10 15:59:01 -0500

    sweeeet

--- Unity/MiniJam121/Assets/Scripts/Behaviours/PlayerBehaviour.cs
+++ Unity/MiniJam121/Assets/Scripts/Behaviours/PlayerBehaviour.cs
@@ -7,11 +7,21 @@ namespace HNS.MiniJam121.Unity.Behaviours
     {
         public float MoveSpeed;
 
+        public ScenerySectionBehaviour[] ScenerySections;
+
+        public int SceneryIndex;
+
         protected bool IsPaused { get; set; }
 
         public void SetPaused(bool isPaused)
         {
             IsPaused = isPaused;
+        }
+
+        public void StartScenery(int index)
+        {
+            SceneryIndex = index;
+            transform.position = ScenerySections[SceneryIndex].StartingPosition;
         }
 
         protected void Update()
@@ -21,25 +31,44 @@ namespace HNS.MiniJam121.Unity.Behaviours
                 return;
             }
 
+            var position = transform.position;
             if (Input.GetKey(KeyCode.A) || 
                 Input.GetKey(KeyCode.LeftArrow))
             {
                 transform.localScale = new Vector3(-1, 1, 1);
-                transform.position -= new Vector3(MoveSpeed, 0, 0) * Time.deltaTime;
+                position -= new Vector3(MoveSpeed, 0, 0) * Time.deltaTime;
             }
 
             if (Input.GetKey(KeyCode.D) ||
                 Input.GetKey(KeyCode.RightArrow))
             {
                 transform.localScale = new Vector3(1, 1, 1);
-                transform.position += new Vector3(MoveSpeed, 0, 0) * Time.deltaTime;
+                position += new Vector3(MoveSpeed, 0, 0) * Time.deltaTime;
             }
+
+            var currentScenery = ScenerySections[SceneryIndex];
+
+            if (position.x < currentScenery.Bounds.x)
+            {
+                position.x = currentScenery.Bounds.x;
+            }
+            else if (position.x > currentScenery.Bounds.y)
+            {
+                position.x = currentScenery.Bounds.y;
+            }
+
+            transform.position = position;
 
             var cameraPosition = Camera.main.transform.position;
 
             cameraPosition = new Vector3(transform.position.x, cameraPosition.y, cameraPosition.z);
 
             Camera.main.transform.position = cameraPosition;
+        }
+
+        protected void Awake()
+        {
+            StartScenery(0);
         }
     }
 }
---
+++ Unity/MiniJam121/Assets/Scripts/Behaviours/ScenerySectionBehaviour.cs
@@ -0,0 +1,12 @@
+namespace HNS.MiniJam121.Unity.Behaviours
+{
+    using UnityEngine;
+
+    [AddComponentMenu("MJ121/ScenerySection")]
+    public class ScenerySectionBehaviour : MonoBehaviour
+    {
+        public SceneryCategory SceneryCategory;
+        public Vector2 Bounds;
+        public Vector2 StartingPosition;
+   }
+}
