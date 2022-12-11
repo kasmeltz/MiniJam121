3b1f08d5b6c2d9c8636411204437ebeff88a7212
Kevin <kasmeltz@lakeheadu.ca>
2022-12-11 15:10:34 -0500

    sweeet

--- Unity/MiniJam121/Assets/Scripts/Models/TriggerItemCategory.cs
+++ Unity/MiniJam121/Assets/Scripts/Models/TriggerItemCategory.cs
@@ -6,5 +6,10 @@ namespace HNS.MiniJam121.Unity.Behaviours
         Closet = 1,
         BathMirror = 2,
         PictureFrame = 3,
+        Locker = 4,
+        Class1 = 5,
+        SchoolBathroom = 6,
+        Class2 = 7,
+        WaterFountain = 8
     }
 }
